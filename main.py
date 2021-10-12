103be892b31d8cf06367ca4d79610f584af8f506
Bruno Cabral <cabral.brc@gmail.com>
2021-10-12 11:48:34 -0700

    added snail

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ clock = pygame.time.Clock()
 sky_surface = pygame.image.load('assets/graphics/Sky.png').convert()
 ground_surface = pygame.image.load('assets/graphics/ground.png').convert()
 
+snail_surface = pygame.image.load('assets/graphics/snail/snail1.png').convert_alpha()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,6 +23,7 @@ while True:
     
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,sky_surface.get_height()))
+    screen.blit(snail_surface,(SCREEN_WIDTH-100,sky_surface.get_height()-snail_surface.get_height()))
 
     pygame.display.update()
     clock.tick(60)
