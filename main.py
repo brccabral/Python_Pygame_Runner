2ac3f7ffdb98dfe3d4b8139c34cc01940e671277
Bruno Cabral <cabral.brc@gmail.com>
2021-10-12 11:44:19 -0700

    add scenario

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
+sky_surface = pygame.image.load('assets/graphics/Sky.png').convert()
+ground_surface = pygame.image.load('assets/graphics/ground.png').convert()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,sky_surface.get_height()))
+
     pygame.display.update()
     clock.tick(60)
