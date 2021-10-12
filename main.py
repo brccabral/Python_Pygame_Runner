485266aa4ca2ace22e66280e7fc6ba4fcad8d3d7
Bruno Cabral <cabral.brc@gmail.com>
2021-10-12 11:41:03 -0700

    add window caption

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption('Runner')
 
 while True:
     for event in pygame.event.get():
